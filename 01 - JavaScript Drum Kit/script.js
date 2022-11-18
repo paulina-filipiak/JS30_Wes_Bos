7d140f0b09bcae27a6d2b414f51a72be28564fe1
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-11-18 12:08:51 +0100

    added queryselector to make key button play audio

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,6 @@
 
 window.addEventListener("keydown", function(e){
-  console.log(e.keyCode)
+  const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.play()
 })
