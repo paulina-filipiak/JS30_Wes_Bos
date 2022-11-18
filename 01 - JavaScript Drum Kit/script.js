b40f1ced63348b131c132bd03d89a95d555e0009
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-11-18 12:22:57 +0100

    added queryselector to catch pressed buttons div and add class .playing

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,9 @@
 
 window.addEventListener("keydown", function(e){
   const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const pressedKey = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
   audio.currentTime =0;
   audio.play()
+  pressedKey.classList.add("playing")
 })
