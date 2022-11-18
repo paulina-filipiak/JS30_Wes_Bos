5394b8c0f24777265af017290eedbb8ac6ef56a6
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-11-18 12:11:39 +0100

    added time = 0 to audio

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -2,5 +2,6 @@
 window.addEventListener("keydown", function(e){
   const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if(!audio) return;
+  audio.currentTime =0;
   audio.play()
 })
